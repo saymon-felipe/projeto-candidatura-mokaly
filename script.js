2d60d4d23430d3f99a2346e7494571e9d50771cb
Saymon Felipe <80544520+saymon-felipe@users.noreply.github.com>
2021-06-27 19:41:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function iniciarDocumento(repetir) {
 
     //FUNÇÃO PARA SORTEAR A QUANTIDADE DE PIXELS DE TRANSLATE
     function pixelsTranslate() {
-        let x = sortear((descobrirLargura()-78))
+        let x = sortear((descobrirLargura()-90))
         if (x < 48) {
             x += 30
         }
